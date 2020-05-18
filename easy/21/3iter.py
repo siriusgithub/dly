d4fc86cf311f9d988fc053d5c0e28ebe98a8d7be
siriusgithub <32184691+siriusgithub@users.noreply.github.com>
2020-05-18 10:50:13 -0700

    Added Testing Comment

--- easy/21/3iter.py
+++ easy/21/3iter.py
@@ -65,7 +65,7 @@ def nextPermutation(givenNumber):
 
     print(int(finalList))
 
-
+# TESTING
 #testNumberList = [69657, 38563, 71025, 82436, 60762, 49886, 28485, 88757, 43451, 84704]
 
 #for testNumber in testNumberList:
